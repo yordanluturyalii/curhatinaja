9e3f553fce78a88eb96ec5157f93daa5b37cb757
yordanluturyalii <yordanluturyali@gmail.com>
2025-03-03 15:44:05 +0700

    feat (config): create drizzle config :wrench:

---
+++ db/schema.ts
@@ -0,0 +1,65 @@
+import { relations } from "drizzle-orm";
+import { integer, pgEnum, pgTable, text, timestamp, varchar } from "drizzle-orm/pg-core";
+
+export const roleEnum = pgEnum('role', ['user', 'ai']);
+export const sentimentEnum = pgEnum('sentiment', ['positive', 'neutral', 'negative']);
+
+export const users = pgTable("users", {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    name: varchar({ length: 255 }).notNull(),
+    username: varchar({length: 255}).notNull().unique(),
+    email: varchar({ length: 255 }).notNull().unique(),
+    password: varchar({length: 255}).notNull()
+});
+
+export const sessions = pgTable("sessions", {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    user_id: integer().references(() => users.id, {onDelete: 'cascade'}).notNull(),
+    created_ad: timestamp()
+});
+
+export const messages = pgTable("messages", {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    session_id: integer().references(() => sessions.id, {onDelete: "cascade"}).notNull(),
+    role: roleEnum().notNull(),
+    message: text().notNull(),
+    created_at: timestamp()
+});
+
+export const results = pgTable('results', {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    session_id: integer().references(() => sessions.id, {onDelete: 'cascade'}).notNull(),
+    sentiment: sentimentEnum().notNull(),
+    summary: text().notNull()
+});
+
+
+export const usersRelations = relations(users, ({one, many}) => ({
+    sessions: many(sessions)
+}));
+
+export const sessionsRelations = relations(sessions, ({one, many}) => ({
+    user: one(users, {
+        fields: [sessions.user_id],
+        references: [users.id]
+    }),
+    messages: many(messages),
+    result: one(results, {
+        fields: [sessions.id],
+        references: [results.session_id]
+    })
+}));
+
+export const messagesRelations = relations(messages, ({one}) => ({
+    session: one(sessions, {
+        fields: [messages.session_id],
+        references: [sessions.id]
+    }),
+}));
+
+export const resultsRelations = relations(results, ({one}) => ({
+    session: one(sessions, {
+        fields: [results.session_id],
+        references: [sessions.id]
+    })
+}));
---
+++ drizzle/0000_workable_justin_hammer.sql
@@ -0,0 +1,36 @@
+CREATE TYPE "public"."role" AS ENUM('user', 'ai');--> statement-breakpoint
+CREATE TYPE "public"."sentiment" AS ENUM('positive', 'neutral', 'negative');--> statement-breakpoint
+CREATE TABLE "messages" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "messages_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"session_id" integer NOT NULL,
+	"role" "role" NOT NULL,
+	"message" text NOT NULL,
+	"created_at" timestamp
+);
+--> statement-breakpoint
+CREATE TABLE "results" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "results_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"session_id" integer NOT NULL,
+	"sentiment" "sentiment" NOT NULL,
+	"summary" text NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "sessions" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "sessions_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"user_id" integer NOT NULL,
+	"created_ad" timestamp
+);
+--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "users_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"name" varchar(255) NOT NULL,
+	"username" varchar(255) NOT NULL,
+	"email" varchar(255) NOT NULL,
+	"password" varchar(255) NOT NULL,
+	CONSTRAINT "users_username_unique" UNIQUE("username"),
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
+ALTER TABLE "messages" ADD CONSTRAINT "messages_session_id_sessions_id_fk" FOREIGN KEY ("session_id") REFERENCES "public"."sessions"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "results" ADD CONSTRAINT "results_session_id_sessions_id_fk" FOREIGN KEY ("session_id") REFERENCES "public"."sessions"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "sessions" ADD CONSTRAINT "sessions_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
