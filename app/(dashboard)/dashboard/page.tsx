5617f0587625e14d39dc102a8e9e41a06f04a030
yordanluturyalii <yordanluturyali@gmail.com>
2025-03-05 15:46:26 +0700

    feat: modal for create session

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,19 +1,30 @@
 'use client';
 
+import NewChatModal from '@/components/new-chat-modal';
 import { Button } from '@/components/ui/button';
-import { signOut, useSession } from 'next-auth/react'
-import React from 'react'
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react'
 
 const Dashboard = () => {
-    const { data } = useSession();
+    const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
+    const router = useRouter();
     return (
-        <div>
-            <h1>Dashboard</h1>
-            <span>{data?.user?.name}</span>
-            <Button variant={"default"} onClick={() => signOut()}>
-                Sign Out
-            </Button>
-        </div>
+        <main className='md:p-10 p-4 sm:p-6 text-gray-900 w-full'>
+            <div className="flex justify-between w-full">
+                <h1 className='text-2xl font-bold'>Dashboard</h1>
+                <Button className='bg-white' onClick={() => setIsOpenModal((val) => !val)}>
+                    New Session
+                </Button>
+            </div>
+
+            <NewChatModal
+                isOpen={isOpenModal}
+                onClose={() => setIsOpenModal(false)}
+                onStart={(settings) => {
+                    router.push(`/chat/new?personality=${settings.personality}&role=${settings.role}`)
+                }}
+            />
+        </main>
     )
 }
 
